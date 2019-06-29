8af783876c4ed22d1474c870568f6b403587cd65
Simon Martín <oleurud@gmail.com>
2019-06-29 12:36:47 +0200

    footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,55 @@
 <template>
   <footer class="footer">
+    <div class="footer-widgets">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <aside class="widget widget-text">
+              <div class="widget-title">
+                <h6>¿Tienes alguna duda?</h6>
+              </div>
+              <div class="textwidget">
+                <p>
+                  <span class="ti-email">hola@pedromartin.pro</span>
+                  <br>
+                  <span class="ti-headphone-alt">+34 975 371 XXX</span>
+                </p>
+                <p>
+                  O puedes escribirnos un mensaje en la <a href="contacto.html">página de contacto</a>.
+                </p>
+              </div>
+            </aside>
+          </div>
+          <div class="col-md-6">
+            <aside class="widget widget-text">
+              <div class="widget-title">
+                <h6>Dirección</h6>
+              </div>
+              <div class="textwidget">
+                <p>
+                  Poligono Industrial Santa Ana
+                  <br>Av. Félix Herrero
+                  <br>Duruelo de la Sierra, Soria
+                  <br>España
+                </p>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="footer-bar">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <div class="copyright">
+            <div class="">
               <p>Pedro Martin, All Rights Reserved.</p>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+        <!-- To top button--><a class="scroll-top" href="#top"><i class="fas fa-angle-up"></i></a>
   </footer>
 </template>
