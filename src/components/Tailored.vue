d10872758474bff02ecef24c752470f1652663f0
Simon Martín <oleurud@gmail.com>
2019-07-02 22:47:16 +0200

    Tailored

---
+++ src/components/Tailored.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <img src="images/shop/1.jpg" alt />
+      </div>
+      <div class="col-lg-5 offset-lg-1">
+        <h2>A medida.</h2>
+        <p
+          class="lead"
+        >Gran parte de nuestro trabajo es crear productos a medida de las necesidades de nuestros clientes.</p>
+        <p
+          class="lead"
+        >Pon a prueba a nuestro equipo de ebanistas. Por muy loco que te parezca, creenos, será dificil que nos sorprenda.</p>
+        <div class="space" data-m-y="50px"></div>
+        <div class="icon-box icon-box-left">
+          <div class="icon-box-icon">
+            <span class="ti-layout"></span>
+          </div>
+          <div class="icon-box-title">
+            <h6>Madera cepillada hasta 4 caras</h6>
+          </div>
+          <div class="icon-box-content">
+            <p>De cualquier medida. De cualquier tipo.</p>
+          </div>
+        </div>
+        <div class="icon-box icon-box-left">
+          <div class="icon-box-icon">
+            <span class="ti-cup"></span>
+          </div>
+          <div class="icon-box-title">
+            <h6>Molduras o ranuras</h6>
+          </div>
+          <div class="icon-box-content">
+            <p>Tenemos una gran cantidad de --- para crear una gran cantidad de moduras o ranuras de diferentes tipos.</p>
+          </div>
+        </div>
+        <div class="icon-box icon-box-left">
+          <div class="icon-box-icon">
+            <span class="ti-harddrives"></span>
+          </div>
+          <div class="icon-box-title">
+            <h6>Rasurados</h6>
+          </div>
+          <div class="icon-box-content">
+            <p>¿Necesitas piezas con un rasurado especial?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Tailored.vue
+++ src/views/Tailored.vue
@@ -16,5 +16,58 @@
       </div>
       <div class="overlay-background" style="opacity: 0.6;"></div>
     </section>
+
+    <section class="module p-b-20">
+      <Tailored></Tailored>
+    </section>
+
+    <section class="module">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-9 m-auto text-center">
+            <h2>Pídenos lo que necesitas.</h2>
+            <p class="lead">Escríbenos un email o llamarnos por teléfono, cuéntanos que necesitas y te haremos un presupuesto lo antes posible.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="icon-box text-center">
+              <div class="icon-box-icon">
+                <span class="ti-email"></span>
+              </div>
+              <div class="icon-box-title">
+                <h6>
+                  <a href="mailto:hola@pedromartin.pro">hola@pedromartin.pro</a>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="icon-box text-center">
+              <div class="icon-box-icon">
+                <span class="ti-headphone-alt"></span>
+              </div>
+              <div class="icon-box-title">
+                <h6 class="d-block d-lg-none">
+                  <a href="tel:+34975371XXX">+34 975 371 XXX</a>
+                </h6>
+                <h6 class="d-none d-lg-block">+34 975 371 XXX</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import Tailored from "@/components/Tailored.vue";
+
+export default {
+  name: "tailored",
+  components: {
+    Tailored
+  }
+};
+</script>
