ffc9b42095d0fe36c3e3290af7506acfaf4bd07f
Simon Martín <oleurud@gmail.com>
2019-07-07 21:31:19 +0200

    dist

---
+++ dist/precache-manifest.9c89c83b7a279bc65e5d705372e333a7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "e01684c4d1a19ab7e2b3bf7fee4eccad",
+    "url": "/js/popper.min.js"
+  },
+  {
+    "revision": "4c06fb7977c9250266de",
+    "url": "/js/app.2b0f082e.js"
+  },
+  {
+    "revision": "c3351888d8c6b99dbad0",
+    "url": "/js/chunk-vendors.f7e5f7c6.js"
+  },
+  {
+    "revision": "1d0a775ef38a5708edb1c3f4827100f5",
+    "url": "/js/plugins.min.js"
+  },
+  {
+    "revision": "4b57cf46dc8cb95c4cca54afc85e9540",
+    "url": "/js/jquery.min.js"
+  },
+  {
+    "revision": "6895e8cd60b62646ce12426015888f58",
+    "url": "/js/bootstrap.min.js"
+  },
+  {
+    "revision": "f5f5059fdc1cb37e488c468c9b18817e",
+    "url": "/js/custom.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/fonts/Simple-Line-Icons.ttf"
+  },
+  {
+    "revision": "d817e1dba5bd5d891d0504bf1715807b",
+    "url": "/images/grabbing.png"
+  },
+  {
+    "revision": "1255a7096ae0b18f0aa06c70dcee7f6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6791b0feb0ae410e70d390114eefd51",
+    "url": "/images/map-icon.png"
+  },
+  {
+    "revision": "ab48cbc1ec25b1b7d00a7aa2cae2efdb",
+    "url": "/images/icons/it.png"
+  },
+  {
+    "revision": "b30fe6bafedcee85dda78c2697daffb0",
+    "url": "/images/shop/1.jpg"
+  },
+  {
+    "revision": "f20afb494deddb4c93321e73fcb064eb",
+    "url": "/images/icons/en.png"
+  },
+  {
+    "revision": "1baf8e0b17a75cf7689c922398af84b8",
+    "url": "/images/icons/pt.png"
+  },
+  {
+    "revision": "c525c005b63d9fbb9dc4eaf1af61bcc9",
+    "url": "/images/icons/es.png"
+  },
+  {
+    "revision": "1e51b01ae75fc86744a5303fba5cc082",
+    "url": "/images/module-2.jpg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/fonts/Simple-Line-Icons.woff"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/fonts/Simple-Line-Icons.woff2"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/fonts/themify.svg"
+  },
+  {
+    "revision": "476ea9bebb888d9cf895c86eb16575aa",
+    "url": "/images/favicon.png"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/fonts/themify.woff"
+  },
+  {
+    "revision": "36f1ffd6c3b593eff6b24c0043f6cc37",
+    "url": "/images/apple-touch-icon.png"
+  },
+  {
+    "revision": "8059e42f2932de485f961d83024ef8af",
+    "url": "/images/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "db364aa253ae8ea959c16d99256826d4",
+    "url": "/images/close.png"
+  },
+  {
+    "revision": "7b3c995243bf8f9d31e145c6157a63d6",
+    "url": "/images/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/fonts/themify.eot"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/fonts/themify.ttf"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/fonts/Simple-Line-Icons.svg"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/fonts/Simple-Line-Icons.eot"
+  },
+  {
+    "revision": "cd8ddad31563129f831658ba1ed06d21",
+    "url": "/css/template.css"
+  },
+  {
+    "revision": "4f2f9256e69b72c4cadef95943499dc3",
+    "url": "/css/plugins.min.css"
+  },
+  {
+    "revision": "c3351888d8c6b99dbad0",
+    "url": "/css/chunk-vendors.188e04d6.css"
+  }
+];
