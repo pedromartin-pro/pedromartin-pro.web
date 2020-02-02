c950db5d26cbe8388d12d9f70dcf276fd6b944e1
Simon <simon@iMac.local>
2020-02-02 11:29:07 +0100

    lint

--- src/components/product/ProductImages.vue
+++ src/components/product/ProductImages.vue
@@ -4,7 +4,7 @@
       <img :src="mainImage" alt />
     </a>
     <ul class="shop-single-item-gallery">
-      <li v-for="image in images">
+      <li v-for="image in images" v-bind:key="image">
         <a class="shop-single-item-popup" :href="image">
           <img :src="image" alt />
         </a>
@@ -23,17 +23,17 @@ export default {
   mounted: function () {
     const el = $(this.$el)
     el.find('.shop-single-item-popup').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0,1]
-        },
-        image: {
-            titleSrc: 'title',
-            tError: 'The image could not be loaded.',
-        }
-    });
+      type: 'image',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        titleSrc: 'title',
+        tError: 'The image could not be loaded.'
+      }
+    })
   }
 }
 </script>
